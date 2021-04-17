3d1c5123b1c56ee921af62dda982fa80ddfaceae
pedro santos <pedrosotnas@gmail.com>
2021-04-17 18:23:43 +0100

    done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+
+  title: {
+    type: String
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: ['Strings']
+  },
+  cuisine: {
+    type: String
+  },
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    minimun: 0
+  },
+  creator: {
+    type: String
+  },
+  created : {
+  type: Date,
+  default: Date.now
+},
+
+  });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
